2c334ea7151b4e7b6809ba761f191f9aac461dd6
kirnjiyun <kimjiyunee@naver.com>
2024-04-01 18:15:39 +0900

    기본세팅

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
